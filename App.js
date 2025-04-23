ad978b2a7f8707e6cc42ef27cb5d70be6c4da965
Nihal SANDHU <nihalsandhu007220@gmail.com>
2025-04-23 18:56:38 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ *    <div id = "parent">
+ *  <div id ="child">
+ * <h1> </h1>
+ * <h2> </h2>
+ * <div>
+ * <div>
+ * ReactElemeny(Object)  render(element )=> HTML(Browser Understands)
+ */
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "1" }, "i am h1 Tag"),
+    React.createElement("h2", { id: "2" }, "i am h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "1" }, "i am h1 Tag"),
+    React.createElement("h2", { id: "2" }, "i am h2 Tag"),
+  ]),
+]);
+// JSX
+console.log(parent); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); // this render methood is beascically converting it into hy1 tag and put it  in inside that
