56f78f64f0e40805c423dba31c837ca5ef1d087e
Nihal SANDHU <nihalsandhu007220@gmail.com>
2025-04-25 15:44:44 +0530

    parcel insatll and understnading what it will do

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
-/**
- *    <div id = "parent">
- *  <div id ="child">
- * <h1> </h1>
- * <h2> </h2>
- * <div>
- * <div>
- * ReactElemeny(Object)  render(element )=> HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { key: "child" }, [
     React.createElement("h1", { id: "1" }, "i am h1 Tag"),
     React.createElement("h2", { id: "2" }, "i am h2 Tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "1" }, "i am h1 Tag"),
-    React.createElement("h2", { id: "2" }, "i am h2 Tag"),
+  React.createElement("div", { key: "child2" }, [
+    React.createElement("h1", { key: "1" }, "this is react"),
+    React.createElement("h2", { key: "2" }, "i am h2 Tag"),
   ]),
 ]);
+
 // JSX
 console.log(parent); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namste React
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching Faster Builds where its caching in parcel-cache supporting hmr
+- Image optimization
+- when you will make production code Minification also done by parcel
+- Bundling
+- Compressing
+- Consisitent Hashing
+- Code splitting
+- Differential Bundling - support older Browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unsused code for you
+- Different Build dev and production
