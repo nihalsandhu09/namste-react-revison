db8f64b3a213146f15c20c27f41a19feffb64af7
Nihal SANDHU <nihalsandhu007220@gmail.com>
2025-05-16 16:53:19 +0530

    jsx

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { key: "child" }, [
-    React.createElement("h1", { id: "1" }, "i am h1 Tag"),
-    React.createElement("h2", { id: "2" }, "i am h2 Tag"),
-  ]),
-  React.createElement("div", { key: "child2" }, [
-    React.createElement("h1", { key: "1" }, "this is react"),
-    React.createElement("h2", { key: "2" }, "i am h2 Tag"),
-  ]),
-]);
 
-// JSX
-console.log(parent); // object
+// React Element
+// React Element is a
+
+// JSX is not html is js it looks like XML syntax
+const jsxHeading = <h1 id="heading ">Namaste React using jsx </h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); // this render methood is beascically converting it into hy1 tag and put it  in inside that
+console.log(jsxHeading);
+root.render(jsxHeading);
